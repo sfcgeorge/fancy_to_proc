e2ebbdd2c5d84b26d954cd55b9e82007eb872f31
Simon George <simon@sfcgeorge.co.uk>
2017-03-30 11:55:07 +0100

    Update for latest Rubocop

--- lib/rubocop/cop/fancy_to_proc/space_around_most_operators.rb
+++ lib/rubocop/cop/fancy_to_proc/space_around_most_operators.rb
@@ -2,17 +2,7 @@
 # frozen_string_literal: true
 begin
   class RuboCop::Cop::Style::SpaceAroundOperators
-    def on_send(node)
-      if node.loc.operator # aref assignment, attribute assignment
-        on_special_asgn(node)
-      elsif !node.unary_operation? && !called_with_dot?(node)
-        op = node.method_name
-        if op != :[] && op != :! && op != :[]= && op != :& && operator?(op)
-          _, _, right, = *node
-          check_operator(node.loc.selector, right.source_range)
-        end
-      end
-    end
+    IRREGULAR_METHODS = %i([] ! []= &).freeze
   end
 rescue NameError
   nil
